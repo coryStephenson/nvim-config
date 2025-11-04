e00e8500c2a7f302eee05297f1de4d8cabd32d2e
Cory Stephenson <coryintenn@gmail.com>
2025-11-04 12:42:49 -0600

    Initial commit

---
+++ init.lua
@@ -0,0 +1,65 @@
+-- Absolute Line Numbers
+vim.cmd("set number")
+
+-- Tabs
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+-- lazy package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "EdenEast/nightfox.nvim" },
+  { "nvim-telescope/telescope.nvim", tag = '0.1.8', dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" }},
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+
+  {
+     "mrcjkb/rustaceanvim",
+     version = '^6', -- Recommended
+     lazy = false, -- This plugin is already lazy
+}
+
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua"},
+  highlight = { enable = true },
+  indent = {enable = true },
+})
+
+-- setup must be called before loading
+require("nightfox.config").set_fox("duskfox")
+require("nightfox").load()
+
+-- Background
+vim.o.background = "dark"
+
+
+
+
+
